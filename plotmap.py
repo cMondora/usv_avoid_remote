985234145c11735e2d5cde69791bfd625f0973f0
王雪纯 <mondora@wangxuechundeMacBook-Pro.local>
2020-12-14 10:47:51 +0800

    add some maps

--- plotmap.py
+++ plotmap.py
@@ -17,15 +17,29 @@ RAD2DEG = 57.29577951308232     # 弧度与角度换算关系1弧度=57.29..角�
 # original big circle small circle-----------
 
 # random small circle-----------
-OBSTACLE_POS = [[12.4, 42.5], [24.5, 13.5], [20.8, 89.3], [71.2, 39.2], [29.8, 56.5], [40.4, 36.3], [70.5, 85.3], [56.4, 68.2], [89.2, 70.5], [83.2, 24.5]]
-OBSTACLE_RADIUS = [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5]
+# OBSTACLE_POS = [[12.4, 42.5], [24.5, 13.5], [20.8, 89.3], [71.2, 39.2], [29.8, 56.5], [40.4, 36.3], [70.5, 85.3], [56.4, 68.2], [89.2, 70.5], [83.2, 24.5]]
+# OBSTACLE_RADIUS = [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5]
 # random small circle-----------
+
+# Big circle one empty-----------
+BIG_RAD = 16
+OBSTACLE_POS = []
+for a in range(10):
+    OBSTACLE_POS.append([50 + BIG_RAD * math.cos(math.pi / 6 * a), 50 + BIG_RAD * math.sin(math.pi / 6 * a)])
+    OBSTACLE_RADIUS = [2.5 for a in range(10)]
+# Big circle one empty-----------
+
+# narrow-----------
+# OBSTACLE_POS = [[24.5, 13.5], [22.8, 79.3], [39.9, 32.5], [26.4, 53.3], [64.5, 85.3], [79.2, 67.5], [74.2, 24.5], [63.4, 45.3]]
+# OBSTACLE_RADIUS = [5, 8, 9.5, 7.5, 5, 9.5, 5.5, 7.5]
+# narrow-----------
 
 for ff in os.listdir('./rlt'):
     cur_file = os.path.join('./rlt', ff)
     data = pd.read_csv(cur_file, sep=',',header=None)
 
     fig = plt.figure()
+    
     ax = fig.add_subplot(111)
 
     for i in range(0, len(data), 2):
@@ -35,7 +49,8 @@ for ff in os.listdir('./rlt'):
     for i in range(0, len(data), 8):
         end_x = data.iloc[i, 0] + 2.5 * math.cos(data.iloc[i, 4])
         end_y = data.iloc[i, 1] + 2.5 * math.sin(data.iloc[i, 4])
-        ax.annotate('', xy=[data.iloc[i, 0], data.iloc[i,1]], xytext=[end_x, end_y], arrowprops=dict(arrowstyle = "->", color=[160 / 255, 191 / 255, 124 / 255]))
+        # ax.annotate('', xy=[data.iloc[i, 0], data.iloc[i,1]], xytext=[end_x, end_y], arrowprops=dict(arrowstyle = "->", color=[160 / 255, 191 / 255, 124 / 255]))
+        ax.annotate('', xy=[end_x, end_y], xytext=[data.iloc[i, 0], data.iloc[i,1]], arrowprops=dict(arrowstyle = "->", color=[160 / 255, 191 / 255, 124 / 255]))
 
 
     # ell1 = Ellipse(xy = (0.0, 0.0), width = 4, height = 8, angle = 30.0, facecolor= 'yellow', alpha=0.3)
@@ -45,8 +60,9 @@ for ff in os.listdir('./rlt'):
         ax.add_patch(cir1)
 
     plt.axis('scaled')
-
-    plt.axis('equal')   #changes limits of x or y axis so that equal increments of x and y have the same length
+    plt.xlim((0,100))
+    plt.ylim((0,100))
+    # plt.axis('equal')   #changes limits of x or y axis so that equal increments of x and y have the same length
 
     # plt.show()
     
